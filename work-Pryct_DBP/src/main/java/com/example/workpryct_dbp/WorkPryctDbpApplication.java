4d6bf0457a2e8eab42ad7433b135d3422a535e8e
Blackmonkcr <aaron.navarro@utec.edu.pe>
2023-10-14 10:51:38 -0500

    Creation proyect files

---
+++ work-Pryct_DBP/src/main/java/com/example/workpryct_dbp/WorkPryctDbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.workpryct_dbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPryctDbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkPryctDbpApplication.class, args);
+	}
+
+}
---
+++ work-Pryct_DBP/src/test/java/com/example/workpryct_dbp/WorkPryctDbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workpryct_dbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkPryctDbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
