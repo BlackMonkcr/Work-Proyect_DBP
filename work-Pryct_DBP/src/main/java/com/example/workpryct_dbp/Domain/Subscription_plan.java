290cfa66e78e7ac9e8429f703f4dc75debd4b2bc
Blackmonkcr <aaron.navarro@utec.edu.pe>
2023-10-15 03:13:21 -0500

    Entity Subscription_plan created definition relation with User

---
+++ work-Pryct_DBP/src/main/java/com/example/workpryct_dbp/Domain/Subscription_plan.java
@@ -0,0 +1,49 @@
+package com.example.workpryct_dbp.Domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "subscription_plans")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Subscription_plan {
+    // ---------------------------------------------------------------------------------------------
+    // Subscription_plan Attributes
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long plan_id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "description", nullable = false, length = 500)
+    private String description;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "plan")
+    private Set<User> users;
+
+    // ---------------------------------------------------------------------------------------------
+    // Constructors (Constructor Default implemented with Lombok)
+
+    public Subscription_plan(String name, Double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // Getters and Setters (Implemented with Lombok)
+    // ---------------------------------------------------------------------------------------------
+}
