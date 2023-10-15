b43c90123fef93d25d0bd547b833e7d4b2423604
Blackmonkcr <aaron.navarro@utec.edu.pe>
2023-10-15 03:12:35 -0500

    Entity Img Created definition relation with User

---
+++ work-Pryct_DBP/src/main/java/com/example/workpryct_dbp/Domain/Img.java
@@ -0,0 +1,55 @@
+package com.example.workpryct_dbp.Domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "imgs")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Img {
+    // ---------------------------------------------------------------------------------------------
+    // Img Attributes
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long img_id;
+
+    @Column(name = "url", nullable = false, length = 250)
+    private String url;
+
+    @Column(name = "alt", nullable = false, length = 50)
+    private String alt;
+
+    @Column(name = "description", nullable = false, length = 500)
+    private String description;
+
+    @Column(name = "upload_date", nullable = false)
+    private Date upload_date;
+
+    @JsonIgnore
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User user;
+
+    // ---------------------------------------------------------------------------------------------
+    // Constructors (Constructor Default implemented with Lombok)
+
+    public Img(String url, String alt, String description, Date upload_date) {
+        this.url = url;
+        this.alt = alt;
+        this.description = description;
+        this.upload_date = upload_date;
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    // Getters and Setters (Implemented with Lombok)
+    // ---------------------------------------------------------------------------------------------
+
+}
