3900c61fecb12733a11bbeed8314eada1b17fdf9
Lisseth Jomely Rondan Cantaro <lissethrondan@MacBook-Air-de-Lisseth.local>
2023-11-03 09:02:02 -0500

    landing version 1

---
+++ MATERIAL-UI/src/components/principalCont/Landing.jsx
@@ -0,0 +1,13 @@
+
+import { Container } from "@mui/material";
+import React from "react";
+
+function Landing() {
+  return (
+    <Container maxWidth="xl" style={{ padding: 0 }}>
+      <img src="../imagenes/Tomato.png" alt="descripcion" style={{ width: "100%"}} />
+    </Container>
+  );
+}
+
+export default Landing;
