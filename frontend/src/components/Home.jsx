d4becb89e434479bcfa938b95827840d3b4dec69
Patricia Nuñez <patricia.nunez@utec.edu.pe>
2023-11-03 23:15:13 -0500

    frontend act.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,15 +6,14 @@ import NavWorkersPreview from "./NavWorkersPreview";
 import NavBarInfo from "./NavBarInfo";
 
 function Home() {
-
   // Get the 3 first users from the database
-
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     Axios({
       method: "GET",
-      url: "http://localhost:8080/user/limits?limit=3",
+      url: "/client/favorite_workers?id=1&limit=3",
+      baseUrl: "work.up.railway.app",
     })
       .then((response) => {
         setUsers(response.data);
@@ -24,11 +23,22 @@ function Home() {
       });
   }, [setUsers]);
 
-
   /*--------------------------RETURN COMPONENT--------------------------*/
 
   return (
     <>
+      {/* Agrega la barra de búsqueda aquí */}
+      <div class="nav">
+  <div class="search-bar">
+    <input type="text" placeholder="Electricista, Pintor, etc..." />
+  </div>
+  <div class="home">
+    <a href="#">Home</a>
+  </div>
+</div>
+
+
+
       <NavBar />
       <NavWorkersPreview
         favoriteWorkers={users}
