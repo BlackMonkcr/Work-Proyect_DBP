38e966f0d0d0955ad04b70b90c1264bd636e2dfe
Aaron Navarro <aaron.navarro@utec.edu.pe>
2023-11-05 13:24:00 -0500

    Update Landing

--- MATERIAL-UI/src/components/navbar.jsx
+++ MATERIAL-UI/src/components/navbar.jsx
@@ -1,25 +1,25 @@
-import { AppBar,Button, Toolbar } from "@mui/material";
+import { AppBar, Toolbar } from "@mui/material";
+import "../css/landingPage.css"
+
 
 export const NavBar = () => {
     
     return(
         <>
         
-        <AppBar position="static" style={{ background: '#9AB4FF', boxShadow: 'none'}}>
+        <AppBar position="static" style={{ background: '#12229D', boxShadow: 'none', display: 'flex', justifyContent: 'center'}}>
 
             <Toolbar >
                 <a className="navbar-brand d-flex align-items-center" href="#">
-                    <img src="/img/logo.png" alt="Logo" width="100" height="30" className="d-inline-block align-text-top rounded rounded-2 mx-3"/>
-                </a>                    
-                <Button className="text-light" sx={{ marginRight: '18px', fontSize: '15px' }} style={{ fontWeight: 'bold'}}>Compañía</Button> 
-                <Button  className="text-light" sx={{ marginRight: '18px', fontSize: '15px' }} style={{ fontWeight: 'bold'}}>Ayuda</Button>  
-                <Button className="text-light" sx={{ marginRight: '18px', fontSize: '15px' }} style={{ fontWeight: 'bold'}}>Contacto</Button> 
+                    <img src="/img/isotipo-2.png" alt="Logo" width="50" height="50" className="d-inline-block align-text-top rounded rounded-2 mx-3"/>
+                </a>
+                <h1 className="text-navbar">Bienvenido a W+ PORTAL</h1>
 
-                <a className="text-decoration-none fw-light mx-2 text-light text-uppercase ms-auto" href="/login">
-                    INICIAR SESION
+                <a className="text-decoration-none fw-light mx-2 text-light ms-auto text-navbar text-navbar-options nv-op-signup" href="/login">
+                    Login
                  </a>
-                 <a className="text-decoration-none  fw-light  mx-2 text-uppercase btn btn-light text-primary mx-2 p-2" href="/register">
-                    REGISTRATE
+                 <a className="text-decoration-none fw-light mx-3 btn btn-light p-1 px-2 text-navbar-options" href="/register">
+                    Sign up
                  </a>
             </Toolbar>
         </AppBar>
--- MATERIAL-UI/src/pages/landingPage.jsx
+++ MATERIAL-UI/src/pages/landingPage.jsx
@@ -1,14 +1,13 @@
 import { NavBar } from "../components/navbar.jsx";
-import { Grid,Button, Container, Box } from '@mui/material';
-import Avatar from '@mui/material/Avatar';
+import { Grid, Container, Box } from '@mui/material';
 import "../css/landingPage.css"
 
 export const Landing = () => {
     return (
     <Container maxWidth={false} style={{position:"static", padding:"0", marginLeft:"0px", marginRight:"0"}} >
         <NavBar/>
-        <Grid container>
-            <Grid item sm={7} lg={5} xl={7} style={{background: 'radial-gradient(farthest-corner circle at 0% 0%, #f3f6f9 100%, #f0f7ff 80%)', maxWidth:'40vw'}} className="box-info"> 
+        <Grid container className="box-img">
+            <Grid item sm={7} lg={5} xl={7} style={{background: '#ededed', maxWidth:'40vw'}} className="box-info"> 
                 <Box className="box-info-contain" style={{margin:"0 50px"}}>
                     <h1 className="title">WORK+</h1>
                     <h2 className="sub-title">Flexibilidad, Independencia & Conexion</h2>
@@ -17,15 +16,10 @@ export const Landing = () => {
                     <button className="bx_containt-button">Comienza a trabajar</button>
                 </Box>
             </Grid>
-    
-            <Grid item sm={5} lg={5} xl={5}  >
-                <Grid container style={{marginTop:'70px'}}>
-                    <Grid item sm={6} lg={6} xl={6} padding={2} className="mx-auto">
-                        <Avatar style={{width:"100%", height:"100%", borderRadius:"0px"}} alt="Travis Howard" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRunT_pAzzZGqRe81m2vBGx5px6TiPyjrvGCbmGJov7CJvkmvY3" />
-                    </Grid>               
-                </Grid>
-            </Grid>
         </Grid>
+        <div className="box-img2">
+
+        </div>
     </Container>
     );
     
