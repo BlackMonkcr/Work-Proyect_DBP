ac4fd445897171b5a2f1764a12d0afc756ad5cf6
Aaron Navarro <aaron.navarro@utec.edu.pe>
2023-11-06 00:47:31 -0500

    Connection Frontend-backend register&login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
+import Home from '../components/Home'
 import '../css/Forms.css';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogin = async (e) => {
@@ -13,66 +13,70 @@ function Login() {
       const response = await fetch(`https://work.up.railway.app/api/v1/auth/signin`, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Type': 'application/json',
         },
-        body: `email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`,
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
       });
 
       const data = await response.text();
 
       if (response.ok) {
-        setMessage('Login successful! Token: ' + data);
         setIsLoggedIn(true);
-      } else {
-        setMessage('Login failed! ' + data);
       }
     } catch (error) {
       setMessage('An error occurred: ' + error.message);
     }
 
-    setUsername('');
+    setEmail('');
     setPassword('');
   }
 
   return (
-    <div className='login'>
-      <div className="login-container">
-        <h2 className="login-title">Log in</h2>
-        <p className="login-description">Log in so you don't miss anything</p>
-        <form className="forms" onSubmit={handleLogin}>
+    <>
+      {isLoggedIn ? (
+           <Home />
+         ):(<div className='login'>
+        <div className="login-container">
+          <h2 className="login-title">Log in</h2>
+          <p className="login-description">Log in so you don't miss anything</p>
+          <form className="forms" onSubmit={handleLogin}>
+            <div>
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="input-field"
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input-field"
+              />
+            </div>
+            <button type="submit" className="btn-login">Log In</button>
+          </form>
           <div>
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="input-field"
-            />
+            <p className="login-prompt">Don't have an account yet?</p>
+            <button to="/signup" className="login-button"><a href='/signUp'>Sign Up</a></button>
           </div>
-          <div>
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="input-field"
-            />
-          </div>
-          <button type="submit" className="btn-login">Log In</button>
-        </form>
-        <div>
-          <p className="login-prompt">Don't have an account yet?</p>
-          <button to="/signup" className="login-button"><a href='/signUp'>Sign Up</a></button>
         </div>
-      </div>
-      <div className="login-img">
-        <h1>
-          <span>Welcome to</span>
-          <br></br>
-          Work+ portal
-        </h1>
-      </div>
-    </div>
+        <div className="login-img">
+          <h1>
+            <span>Welcome to</span>
+            <br></br>
+            Work+ portal
+          </h1>
+        </div>
+      </div>)}
+    </>
   );
 }
 
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../css/Forms.css';
+import Login from './Login.jsx'
 
 function Signup() {
   const [username, setUsername] = useState('');
@@ -9,9 +10,38 @@ function Signup() {
   const [name, setName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [address, setAddress] = useState('');
+  const [isSignUp, setIsSignUp] = useState(false);
 
-  const handleSignup = (e) => {
+  const handleSignup = async (e) => {
     e.preventDefault();
+    
+    try {
+      const response = await fetch(`https://work.up.railway.app/api/v1/auth/signup`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: name,
+          username: username,
+          password: password,
+          phoneNumber: phoneNumber,
+          district: district,
+          precise_location: address,
+          email: email,
+          password: password
+        }),
+      });
+
+      const data = await response.text();
+
+      if (response.ok) {
+        setIsSignUp(true);
+      }
+    } catch (error) {
+      setMessage('An error occurred: ' + error.message);
+    }
+    
     setUsername('');
     setEmail('');
     setPassword('');
@@ -22,91 +52,95 @@ function Signup() {
   }
 
   return (
-    <div className='register'>
-      <div className="register-container">
-        <h2 className="register-title">Sign Up</h2>
-        <p className="register-description">Sign up to find the best workers for your home!</p>
-        <form className="forms" onSubmit={handleSignup}>
+    <>
+      {isSignUp ? (
+        <Login />
+         ):(<div className='register'>
+        <div className="register-container">
+          <h2 className="register-title">Sign Up</h2>
+          <p className="register-description">Sign up to find the best workers for your home!</p>
+          <form className="forms" onSubmit={handleSignup}>
+            <div>
+              <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="input-field"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="input-field"
+              />
+            </div>
+            <div>
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="input-field"
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input-field"
+              />
+            </div>
+            <div>
+              <input
+                type="tel"
+                placeholder="Phone Number"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                className="input-field"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="District"
+                value={district}
+                onChange={(e) => setDistrict(e.target.value)}
+                className="input-field"
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                placeholder="Address"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                className="input-field"
+              />
+            </div>
+            <button type="submit" className="btn-register">
+              Sign Up
+            </button>
+          </form>
           <div>
-            <input
-              type="text"
-              placeholder="Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="input-field"
-            />
+            <p className="register-prompt">Do you have an account?</p>
+            <button className="login-button"><a href='/login'>Login</a></button>
           </div>
-          <div>
-            <input
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="input-field"
-            />
-          </div>
-          <div>
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="input-field"
-            />
-          </div>
-          <div>
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="input-field"
-            />
-          </div>
-          <div>
-            <input
-              type="tel"
-              placeholder="Phone Number"
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-              className="input-field"
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              placeholder="District"
-              value={district}
-              onChange={(e) => setDistrict(e.target.value)}
-              className="input-field"
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              placeholder="Address"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              className="input-field"
-            />
-          </div>
-          <button type="submit" className="btn-register">
-            Sign Up
-          </button>
-        </form>
-        <div>
-          <p className="register-prompt">Do you have an account?</p>
-          <button className="login-button"><a href='/login'>Login</a></button>
         </div>
-      </div>
-      <div className="register-img">
-        <h1>
-          <span>Welcome to</span>
-          <br></br>
-          Work+ portal
-        </h1>
-      </div>
-    </div>
+        <div className="register-img">
+          <h1>
+            <span>Welcome to</span>
+            <br></br>
+            Work+ portal
+          </h1>
+        </div>
+      </div>)}
+    </>
   );
 }
 
