4b5b535f9bcad984f3f96c3554f3a266556f68c6
Diana Ñañez <dianananez6@gmail.com>
2023-11-07 13:01:16 -0500

    DTO WORKERINFORMATION

---
+++ backend/src/main/java/com/example/workpryct_dbp/DTO/response/WorkerInformation.java
@@ -0,0 +1,28 @@
+package com.example.workpryct_dbp.DTO.response;
+
+import com.example.workpryct_dbp.Domain.Worker;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class WorkerInformation{
+    private Long id;
+    private String name;
+    private String occupation;
+    private String city;
+    private String keyProfilePicture;
+    private Double rating;
+
+    public WorkerInformation(Worker worker) {
+        this.id = worker.getWorker_id();
+        this.name = worker.getUser().getName();
+        this.occupation = worker.getOccupation();
+        this.city = worker.getUser().getCity();
+        this.keyProfilePicture = worker.getUser().getProfile_picture().getAlt();
+        this.rating = worker.getUser().getRating();
+    }
+
+    public WorkerInformation() {}
+}
