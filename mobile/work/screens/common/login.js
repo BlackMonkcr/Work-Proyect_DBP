603d84fd3db25d55141a0649283b6a5da27a907b
Diana Ñañez <dianananez6@gmail.com>
2023-11-17 00:25:05 -0500

    login in progress

---
+++ mobile/work/screens/common/login.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const LoginScreen = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Aquí puedes realizar la lógica de autenticación con el servidor
+    console.log('Inicio de sesión exitoso');
+    // Puedes navegar a otra pantalla después del inicio de sesión
+    navigation.navigate('Home');
+    //Realizar la logica si es cliente o worker
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Inicio de Sesión</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nombre de usuario"
+        onChangeText={(text) => setUsername(text)}
+        value={username}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+      />
+      <Button title="Iniciar Sesión" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingLeft: 8,
+  },
+});
+
+export default LoginScreen;
