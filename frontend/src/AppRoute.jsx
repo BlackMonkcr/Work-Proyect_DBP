47f6163b330c3f4842417aaffdd67808751626d4
Patricia Nuñez <patricia.nunez@utec.edu.pe>
2023-11-18 17:04:54 -0500

    routes update

--- frontend/src/AppRoute.jsx
+++ frontend/src/AppRoute.jsx
@@ -7,7 +7,8 @@ import Login from "./pages/Login";
 import Landing from "./pages/landingPage";
 import LoginWorker from "./pages/LoginWorker";
 import SignupWorker from "./pages/SignupWorker";
-import HomeClient from "./pages/Home";
+import HomeClient from "./pages/HomeClient";
+import HomeWorker from "./pages/HomeWorker";
 
 export default function App() {
     return (
@@ -18,7 +19,8 @@ export default function App() {
                 <Route path="/signUp" element={<Signup/>} />
                 <Route path="/loginWorker" element={<LoginWorker/>} />
                 <Route path="/signupWorker" element={<SignupWorker/>} />
-                <Route path="/home" element={<HomeClient/>}/>
+                <Route path="/homeClient" element={<HomeClient/>}/>
+                <Route path="/homeWorker" element={<HomeWorker/>}/>
             </Routes>
             <NotificationContainer />
         </BrowserRouter>
