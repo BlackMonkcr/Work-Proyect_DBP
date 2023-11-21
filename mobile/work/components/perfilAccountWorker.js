718d69f4695df76ef801a19a118d6c52f47b5867
Diana Ñañez <dianananez6@gmail.com>
2023-11-21 15:05:26 -0500

    profile worker

--- mobile/work/components/perfilAccountWorker.js
+++ mobile/work/components/perfilAccountWorker.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import React,{useState} from 'react';
+import { View, Text, StyleSheet, Image, ScrollView,TouchableOpacity,TextInput } from 'react-native';
 
 const ProfileScreen = () => {
   const userProfile = {
@@ -13,8 +13,20 @@ const ProfileScreen = () => {
     profileImage: 'https://placekitten.com/200/200', // URL de la imagen de perfil
   };
 
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedDescription, setEditedDescription] = useState(userProfile.description);
+  const [editedSkills, setEditedSkills] = useState(userProfile.skills.join(', '));
+
+  const handleSaveChanges = () => {
+    // Aquí puedes implementar la lógica para guardar los cambios en la descripción y habilidades
+    console.log('Guardando cambios...');
+    setIsEditing(false);
+  };
+
+
+
   return (
-    <ScrollView style={styles.container}>
+  <ScrollView style={styles.container}>
       <View style={styles.header}>
         <Image source={{ uri: userProfile.profileImage }} style={styles.profileImage} />
         <Text style={styles.name}>{userProfile.name}</Text>
@@ -31,18 +43,48 @@ const ProfileScreen = () => {
         </View>
         <View style={styles.attribute}>
           <Text style={styles.headerInfo}>Description: </Text>
-          <Text style={styles.bodyInfo}>{userProfile.description}</Text>
+          {isEditing ? (
+            <TextInput
+              style={styles.editableText}
+              value={editedDescription}
+              onChangeText={setEditedDescription}
+              multiline={true}
+              numberOfLines={4}
+            />
+          ) : (
+            <Text style={styles.bodyInfo}>{userProfile.description}</Text>
+          )}
         </View>
+
         <View style={styles.attribute}>
           <Text style={styles.headerInfo}>Skills: </Text>
-          <View style={styles.skillsContainer}>
-            {userProfile.skills.map((skill, index) => (
-              <Text key={index} style={styles.skill}>
-                {skill}
-              </Text>
-            ))}
-          </View>
+          {isEditing ? (
+            <TextInput
+              style={styles.editableText}
+              value={editedSkills}
+              onChangeText={setEditedSkills}
+              multiline={true}
+              numberOfLines={4}
+            />
+          ) : (
+            <Text style={styles.bodyInfo}>{userProfile.skills.join(', ')}</Text>
+          )}
         </View>
+
+        {isEditing && (
+          <TouchableOpacity style={styles.saveButton} onPress={handleSaveChanges}>
+            <Text style={styles.saveButtonText}>Save Changes</Text>
+          </TouchableOpacity>
+        )}
+
+        {!isEditing && (
+          <TouchableOpacity
+            style={styles.editButton}
+            onPress={() => setIsEditing(true)}
+          >
+            <Text style={styles.editButtonText}>Edit Profile</Text>
+          </TouchableOpacity>
+        )}
       </View>
     </ScrollView>
   );
@@ -51,58 +93,98 @@ const ProfileScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0f0f0',
+    backgroundColor: '#ededed',
   },
   header: {
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
     backgroundColor: '#3672F5',
+    borderRadius: 30,
   },
   profileImage: {
     width: 150,
     height: 150,
     borderRadius: 75,
+    marginTop: '20%',
     marginBottom: 10,
   },
   name: {
-    fontSize: 24,
+    fontSize: 27,
     fontWeight: 'bold',
     color: '#fff',
   },
-  occupation: {
-    fontSize: 18,
+  occupation:{
+    fontSize: 20,
+    fontWeight: 'bold',
     color: '#fff',
   },
   body: {
-    padding: 20,
+    padding: 30,
+    height: '100%',
   },
   attribute: {
     flexDirection: 'column',
+    alignItems: 'left',
     marginTop: 20,
   },
   headerInfo: {
-    fontSize: 18,
+    fontSize: 22,
     fontWeight: 'bold',
-    color: '#333',
+    color: '#0fb088',
   },
   bodyInfo: {
-    fontSize: 16,
+    marginTop:8,
+    fontSize: 18,
     color: '#555',
     marginLeft: 5,
   },
   skillsContainer: {
     flexDirection: 'column',
     flexWrap: 'wrap',
-    marginTop: 5,
+    marginTop: 8,
   },
   skill: {
     color: '#555',
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    margin: 5,
-  }
+    fontSize: 18,
+    paddingHorizontal: 5,
+    margin: 2,
+  },
+  editableText: {
+    fontSize: 16,
+    color: '#555',
+    marginLeft: 5,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 5,
+    marginBottom: 10,
+    width: 350,
+    height: 200,
+  },
+  saveButton: {
+    backgroundColor: '#3672F5',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  saveButtonText: {
+    fontWeight: 'bold',
+    fontSize:18,
+    color: '#fff',
+    textAlign: 'center',
+  },
+  editButton: {
+    backgroundColor: '#3672F5',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  editButtonText: {
+    fontWeight: 'bold',
+    fontSize:18,
+    color: '#fff',
+    textAlign: 'center',
+  },
 });
 
 export default ProfileScreen;
