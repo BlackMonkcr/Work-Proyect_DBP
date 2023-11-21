a3ba12e1f2a7693f916ae90bc6db0cbff880c4f2
Diana Ñañez <dianananez6@gmail.com>
2023-11-21 15:15:51 -0500

    client??

--- mobile/work/screens/worker/homescreenWorker.js
+++ mobile/work/screens/worker/homescreenWorker.js
@@ -1,12 +1,68 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React,{useState, useEffect} from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import WorkerCard from '../../components/workerCard';
 
 const HomeScreenWorker = () => {
+    const navigation = useNavigation();
+    const [workerData, setWorkerData] = useState([]);
+    const colors = ['#3837F5', '#3672F5', '#36AAB5', '#7436F5'];
+
+    useEffect(() => {
+      fetch('https://work.up.railway.app/worker/homeCards?limit=10', {
+        method: 'GET',
+      })
+        .then((response) => response.json())
+        .then((data) => setWorkerData(data))
+        .catch((error) => console.error(error));
+    }, []);
+
+    React.useLayoutEffect(() => {
+      navigation.setOptions({
+        headerShown: false,
+      });
+    }, [navigation]);
+
     return (
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
         <View>
-            <Text>Welcome to the Worker Home Screen!</Text>
+          <View style={styles.header}>
+            <Text style={styles.title}>Home</Text>
+          </View>
+          {workerData.map((worker) => (
+            <WorkerCard
+              key={worker.id}
+              name={worker.name}
+              rating={worker.rating}
+              description={worker.description}
+              color={colors[index % colors.length]}
+            />
+          ))}
         </View>
+      </ScrollView>
     );
-};
+  };
+
+  const styles = StyleSheet.create({
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: '#333',
+      textAlign: 'left',
+      marginTop: 5,
+    },
+    scrollViewContent: {
+      backgroundColor: '#ededed',
+      paddingBottom: 20,
+    },
+    header: {
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      marginLeft: 20,
+      marginTop: 50,
+      gap: 15,
+    },
+  });
 
 export default HomeScreenWorker;
