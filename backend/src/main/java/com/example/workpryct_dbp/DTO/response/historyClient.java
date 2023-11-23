61b68194c4bff3490a20e183f5167ef3e14c6130
Blackmonkcr <aaron.navarro@utec.edu.pe>
2023-11-23 11:35:34 -0500

    key profile picture

--- backend/src/main/java/com/example/workpryct_dbp/DTO/response/historyClient.java
+++ backend/src/main/java/com/example/workpryct_dbp/DTO/response/historyClient.java
@@ -13,10 +13,18 @@ public class historyClient {
     private Long id;
     private String name;
     private String email;
+    private String keyProfilePicture;
 
     public historyClient(Client client) {
         this.id = client.getClient_id();
         this.name = client.getUser().getName();
         this.email = client.getUser().getEmail();
+        this.keyProfilePicture = client.getUser().getName();
+        for (int i = 0; i < this.keyProfilePicture.length(); i++) {
+            if (this.keyProfilePicture.charAt(i) == ' ') {
+                this.keyProfilePicture = this.keyProfilePicture.substring(0, i) + this.keyProfilePicture.substring(i + 1);
+                i--;
+            }
+        }
     }
 }
